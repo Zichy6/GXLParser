e023cdf442612441e856427c58f7c441ab6ff7f8
Zichy6 <zich.michal@gmail.com>
2014-11-13 15:55:35 +0100

    First commit.

---
+++ GXLParser/GXL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GXLParser
+{
+    class GXL
+    {
+        public static readonly string SYSTEM_ID = "http://www.gupro.de/GXL/gxl-1.0.dtd";
+        /** The NODE element name, "node". */
+        public static readonly string NODE = "node";
+        /** The EDGE element name, "edge". */
+        public static readonly string EDGE = "edge";
+        /** The GRAPH element name, "graph". */
+        public static readonly string GRAPH = "graph";
+    }
+}
